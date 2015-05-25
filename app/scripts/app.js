799a47520bfafc8f13bc85d6d0c2cc99d8e00ff5
Vincent Guillou <vin.guillou@gmail.com>
2015-05-25 19:35:00 +0200

    Changing to MV* implementation, adding error handling

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,12 @@
 (function(window, document, undefined) {
   'use strict';
-  
+
   // Global variables
   window.defaultColor = '#00BCD4';
 
   // Setup the service worker
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./serviceworker.js', { scope: './' })
+    navigator.serviceWorker.register('./serviceworker.js', {scope: './'})
       .then(function(r) {
         console.log('Registered service worker with scope: ' + r.scope);
       })
